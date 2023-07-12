34178b182df4f11a3dff966fe8371c1ed49ae927
heexohee <rsoy2918@naver.com>
2023-07-13 01:23:25 +0900

    configstore 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,14 +1,30 @@
-import { createStore } from "redux";
-import { combineReducers } from "redux";
-import todos from "../modules/todos";
+// --- 기존의 코드 ---
+
+//import { createStore } from "redux";
+//import { combineReducers } from "redux";
 
 // 1. create rootReducer with reducers
-const rootReducer = combineReducers({
-  todos,
-});
+// const rootReducer = combineReducers({
+//   todos,
+// });
 
 // 2. create store
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
+
+// 3. export
+//export default store;
+
+
+// --- 툴킷 사용 ---
+import todos from "../modules/todos";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer : {
+    todos,
+  }
+});
 
 // 3. export
 export default store;
+
